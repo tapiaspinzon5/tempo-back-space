e5f4ecc8260c7b7690d8217777891d4ffc3cad92
Alejandro Tapias <Diego.TapiasPinzon@teleperformance.com>
2022-01-20 14:10:44 -0500

    getTemplatesLoaded updated

--- server/controllers/routes.controller.js
+++ server/controllers/routes.controller.js
@@ -338,14 +338,85 @@ exports.getActivitiesTL = async (req, res) => {
 exports.getTemplatesLoaded = async (req, res) => {
 
   const { caso } = req.body;
+  const nameArray = [];
 
   sql
     .query(
       "spQueryLoadTemplate",
-      parametros({ idccms: req.query.idccms, caso }, "spQueryLoadTemplate")
-    )
+      parametros({ idccms: req.query.idccms, caso }, "spQueryLoadTemplate"))
     .then((result) => {
-      responsep(1, req, res, result);
+
+      // Si el caso solicitado es el 2 tenemos que filtrar.
+      if (caso === 2) {
+
+        // Array con solo los nombres
+        result.forEach(element => {
+          nameArray.push(element.Nombre);
+        });
+        
+        // Array filtrado con los nombres sin repetir
+        const resultado = nameArray.filter((item,index)=>{
+          return nameArray.indexOf(item) === index;
+        })
+  
+        // Agrupamos los elementos de la respuesta (result) por nombre; 
+        let filteredData= resultado.map(el => {
+  
+          let tempArray = []
+  
+          result.forEach(element => {
+            if (element.Nombre === el) tempArray.push(element) 
+          });
+  
+          return tempArray;
+        })
+  
+        // Recorremos el nuevo array (el que agrupa por nombre) para conocer cuantos de cada lideres tiene
+        let objResponse = filteredData.map(el => {
+  
+          let name = el[0]?.Nombre;
+          let cantTL = 0;
+          let cantRL = 0;
+          let cantQAL = 0;
+          let cantOM = 0;
+  
+          el.forEach(elemnt => {
+            
+            switch (elemnt?.RoleAgent) {
+              case 'Team Leader':
+                cantTL=elemnt?.Total
+                break;
+  
+              case 'Reporting Lead':
+                cantRL=elemnt?.Total
+                break;
+  
+              case 'QA Lead':
+                cantQAL=elemnt?.Total
+                break;
+  
+              case 'Operation Manager':
+                cantOM=elemnt?.Total
+                break;
+            
+              default:
+                break;
+            }
+          });
+  
+          return ({
+            "nombre": name,
+            "teamLeads": cantTL,
+            "reportingLeads": cantRL,
+            "QALeads":cantQAL,
+            "OpsManagers":cantOM
+          })
+        })      
+  
+        responsep(1, req, res, objResponse);
+      } else {
+        responsep(1, req, res, result);
+      }
     })
     .catch((err) => {
       console.log(err, "sp");
