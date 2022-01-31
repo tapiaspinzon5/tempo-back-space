b4512166760f415dd2980152c3ad6405202e1a8a
Alejandro Tapias <tapiaspinzon.5@gmail.com>
2022-01-31 12:05:36 -0500

    postChangeRol added

--- server/routes/router.js
+++ server/routes/router.js
@@ -30,26 +30,31 @@ module.exports = (router) => {
   router.post("/getquizbyagent", routes.getQuizByAgent); // Lista todos los examenes asignados por cssmid del agente.
   router.post("/getQuizDetail", routes.getQuizDetail);   // Retorna las preguntas y respuestas de un examen por ID del examen.
   router.post("/getresultquiz", routes.getResultQuiz);   // Recibe las respuestas seleccionadas por el agente y retorna los resultados del examen.
-  router.post("/getchanllenges", routes.getChanllenges);   // Retorna las actividades por categoria y stage.
+   // Retorna los retos tanto para el TL (para asiganarlo a un agente) como para los agentes(cuando se retan entre ellos)
+  router.post("/getchanllenges", routes.getChanllenges);  
   router.post("/gettemplatesloaded", routes.getTemplatesLoaded);   // Retorna la cantidad de registros cargados en cada plantilla.
   router.post("/getloadinstructions", routes.getLoadInstructions);   // Retorna la cantidad de registros cargados en cada plantilla.
   router.post("/getagentschallengeassignmenttl", routes.getAgentsChallengeAssignmentTL);   // Retorna la cantidad de registros cargados en cada plantilla.
   router.post("/getactivitiesviewagent", routes.getActivitiesViewAgent);   // Retorna la cantidad de registros cargados en cada plantilla.
   router.post("/getactivitiesdescriptionagent", routes.getActivitiesDescriptionAgent);   // Retorna la cantidad de registros cargados en cada plantilla.
-
+  
   // Descargar archivos
   // http://localhost:4343/api/gettemplate/SuperUser.csv
   router.get("/gettemplate/:name", routes.getTemplate);   // Recibe las respuestas seleccionadas por el agente y retorna los resultados del examen.
   
-// Asignar challenges
+  // Asignar challenges
   router.post("/postassignactivitiestl",routes.assignActivitiesTL);
-
+  
   router.post("/sendemailnotification", routes.sendEmailNotification);
-
+  
   // Ruta de prueba para enviar notificaciones.
   router.post("/sendfcmnotification", routes.sendFCMNotificacion);
   
   
+
+
+  // TODO: Borrar este endpoint
+  router.post("/postchangerol", routes.postChangeRol);   // Retorna las actividades por categoria y stage.
   //CRUD
   MapSpRouter("/sqlget", "spGetCentral");
   MapSpRouter("/sqlupdate", "spUpdateCentral");
