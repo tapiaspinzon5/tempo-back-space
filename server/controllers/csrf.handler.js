fb7bb657db3e4a065ef492fd72a4b29f2f526016
Alejandro Tapias <tapiaspinzon.5@gmail.com>
2022-02-02 14:49:20 -0500

    set cors and cookies config for development

--- server/controllers/csrf.handler.js
+++ server/controllers/csrf.handler.js
@@ -5,23 +5,23 @@ let exceptionHandler = (req, res, next) => {
     if (properties.ENV == 'Production'){
         if (req.url === '/api/test' || req.url === '/api/ccmslogin' || req.url === '/login' || req.url === '/container/main') {    
             // Habilitar cuando se vaya a pasar a produccion
-            const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
+            // const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
             // Habilita cuando se trabaja en desarrollo
-            // const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
+            const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
             csrfProtection(req, res, next);
         } else {
 
             // Habilitar cuando se vaya a pasar a produccion
-            const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  } });
+            // const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  } });
             // 
-            // const csrfProtection = csrf({ cookie: true});
+            const csrfProtection = csrf({ cookie: true});
             csrfProtection(req, res, next)
         }
     } else {
         // Habilitar cuando se vaya a pasar a produccion
-        const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
+        // const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
         // 
-        // const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
+        const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
         csrfProtection(req, res, next);
     }
 }
