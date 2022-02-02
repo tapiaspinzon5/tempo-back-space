0076708aee984593a252f62089b5f2562215f273
Alejandro Tapias <tapiaspinzon.5@gmail.com>
2022-02-02 14:10:40 -0500

    Security protocols updated

--- server/controllers/csrf.handler.js
+++ server/controllers/csrf.handler.js
@@ -3,15 +3,25 @@ const properties = require('../properties/properties')
 
 let exceptionHandler = (req, res, next) => {
     if (properties.ENV == 'Production'){
-        if (req.url === '/api/test' || req.url === '/api/ccmslogin' || req.url === '/login' || req.url === '/container/main') {
-            const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
+        if (req.url === '/api/test' || req.url === '/api/ccmslogin' || req.url === '/login' || req.url === '/container/main') {    
+            // Habilitar cuando se vaya a pasar a produccion
+            const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
+            // Habilita cuando se trabaja en desarrollo
+            // const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
             csrfProtection(req, res, next);
         } else {
-            const csrfProtection = csrf({ cookie: true});
+
+            // Habilitar cuando se vaya a pasar a produccion
+            const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  } });
+            // 
+            // const csrfProtection = csrf({ cookie: true});
             csrfProtection(req, res, next)
         }
     } else {
-        const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
+        // Habilitar cuando se vaya a pasar a produccion
+        const csrfProtection = csrf({ cookie: { httpOnly: true, sameSite: 'strict', secure: true  }, ignoreMethods: ['POST', 'GET'] });
+        // 
+        // const csrfProtection = csrf({ cookie: true, ignoreMethods: ['POST', 'GET'] });
         csrfProtection(req, res, next);
     }
 }
