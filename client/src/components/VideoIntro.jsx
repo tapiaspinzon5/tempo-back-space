5f1515fd2344e1367712b976177a860132d57b58
Daniel Moreno <Daniel.MorenoSalas@teleperformance.com>
2022-02-04 09:16:51 -0500

    Video Introductorio

--- client/src/components/VideoIntro.jsx
+++ client/src/components/VideoIntro.jsx
@@ -1,32 +1,15 @@
-import { height } from "@mui/system";
 import React from "react";
-import {
-  Player,
-  ControlBar,
-  ReplayControl,
-  ForwardControl,
-  CurrentTimeDisplay,
-  TimeDivider,
-  PlaybackRateMenuButton,
-  VolumeMenuButton,
-} from "video-react";
-import "video-react/dist/video-react.css";
-
+import ReactPlayer from "react-player";
 export const VideoIntro = () => {
-  let config = { full: false, h: "90%" };
   return (
     <>
-      <Player fluid={config.full} height={600} width={1100}>
-        <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-        <ControlBar>
-          <ReplayControl seconds={10} order={1.1} />
-          <ForwardControl seconds={30} order={1.2} />
-          <CurrentTimeDisplay order={4.1} />
-          <TimeDivider order={4.2} />
-          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-          <VolumeMenuButton disabled />
-        </ControlBar>
-      </Player>
+      <ReactPlayer
+        style={{ border: "solid 6px blue", borderRadius: "10px" }}
+        width="70%"
+        height="90%"
+        controls
+        url="https://www.youtube.com/watch?v=DEDjF2tLbhk"
+      />
     </>
   );
 };
--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   HashRouter as Router,
   Routes,
@@ -9,7 +9,8 @@ import { useSelector } from "react-redux";
 
 import HomeUser from "../pages/HomeUser";
 import { Navbar } from "../components/SideBar/Navbar";
-import { Grid, styled } from "@mui/material";
+import { Grid, Button, styled, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { QuizViewV2 } from "../pages/QuizViewV2";
 import TeamsProgress from "../pages/TeamsProgress";
 import UpQuiz from "../pages/UpQuiz";
@@ -28,6 +29,7 @@ import FollowingTeamsKPI from "../pages/TeamLeader/FollowingTeamsKPI";
 import ChallengeAssignment from "../pages/TeamLeader/ChallengeAssignment";
 import BadgeManagement from "../pages/TeamLeader/BadgeManagement";
 import ActivitiesDescription from "../components/Agents/activitiesview/ActivitiesDescription";
+import { VideoIntro } from "../components/VideoIntro";
 //import { Star5 } from "./Star 5/Star5";
 //import { QuizView } from "../pages/QuizView";
 //import { Description } from "../pages/Description";
@@ -38,90 +40,136 @@ const MainApp = styled(Grid)(() => ({
   display: "flex",
   //position: "relative",
 }));
+const MainHomevideo = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "95vh",
+  width: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: "10px",
+}));
 
 const AppRouter = () => {
   const userData = useSelector((store) => store.loginUser.userData);
+  const [video, setVideo] = useState(true);
   const [navView, setNavView] = useState(true);
-
+  const theme = useTheme();
+  const handleView = () => {
+    setVideo(false);
+  };
+  useEffect(() => {
+    if (userData?.NumberLogins !== 1) {
+      setVideo(false);
+    }
+  }, []);
   return (
     <Router>
       <MainApp sx={{ bgcolor: "background.default" }}>
-        {userData?.Role && navView && <Navbar />}
+        {userData?.NumberLogins === 1 ? (
+          <MainHomevideo>
+            <VideoIntro />
+            <Box
+              sx={{
+                margin: "0 15px 0 15px",
+                display: "flex",
+                justifyContent: "center",
+                padding: "0 2rem 2rem 0",
+              }}
+            >
+              <Button
+                onClick={handleView}
+                sx={{
+                  background: theme.palette.background.primary,
+                  color: "#FFFFFF",
+                  margin: "10px",
+                  width: "160px",
+                }}
+              >
+                Continue
+              </Button>
+            </Box>
+          </MainHomevideo>
+        ) : (
+          userData?.Role && navView && <Navbar />
+        )}
 
-        <Routes>
-          {userData?.Role === "Agent" && (
-            <>
-              <Route path="/" element={<Navigate to="/homeusers" />} />
-              <Route path="/homeusers" element={<HomeUser />} />
-              <Route path="/activitiesview" element={<ActivitiesView />} />
-              <Route
-                path="/activitiesview/:idActivity/:context"
-                element={<ActivitiesDescription />}
-              />
-              <Route
-                path="/quiz/:idquiz"
-                element={<QuizViewV2 setNavView={setNavView} />}
-              />
-              <Route
-                path="/quizdetails/:idquiz/:stateActivity"
-                element={<QuizDetails />}
-              />
-            </>
-          )}
+        {userData?.NumberLogins !== 1 && (
+          <Routes>
+            {userData?.Role === "Agent" && (
+              <>
+                <Route path="/" element={<Navigate to="/homeusers" />} />
+                <Route path="/homeusers" element={<HomeUser />} />
+                <Route path="/activitiesview" element={<ActivitiesView />} />
+                <Route
+                  path="/activitiesview/:idActivity/:context"
+                  element={<ActivitiesDescription />}
+                />
+                <Route
+                  path="/quiz/:idquiz"
+                  element={<QuizViewV2 setNavView={setNavView} />}
+                />
+                <Route
+                  path="/quizdetails/:idquiz/:stateActivity"
+                  element={<QuizDetails />}
+                />
+              </>
+            )}
 
-          {userData?.Role === "Operation Manager" && (
-            <>
-              <Route path="/" element={<Navigate to="/homeom" />} />
-              <Route path="/homeom" element={<HomeOM />} />
-              <Route path="/upcampaign" element={<UpCampaign />} />
-            </>
-          )}
-          {userData?.Role === "QA Lead" && (
-            <>
-              <Route path="/" element={<Navigate to="/homeqal" />} />
-              <Route path="/upquiz" element={<UpQuiz />} />
-              <Route path="/homeqal" element={<HomeQAL />} />
-            </>
-          )}
-          {userData?.Role === "Reporting Lead" && (
-            <>
-              <Route path="/" element={<Navigate to="/homerl" />} />
-              <Route path="/homerl" element={<HomeRL />} />
-              <Route path="/upagents" element={<UpAgents />} />
-            </>
-          )}
-          {userData?.Role === "Super Admin" && (
-            <>
-              <Route path="/" element={<Navigate to="/homesa" />} />
-              <Route path="/homesa" element={<HomeSA />} />
-              <Route path="/upcount" element={<UpCount />} />
-            </>
-          )}
-          {userData?.Role === "Team Leader" && (
-            <>
-              <Route path="/" element={<Navigate to="/hometl" />} />
-              <Route path="/hometl" element={<HomeTL />} />
-              <Route path="/homeusers" element={<HomeUser />} />
-              <Route path="/followingteams" element={<FollowingTeamsKPI />} />
-              <Route
-                path="/challengeasignment"
-                element={<ChallengeAssignment />}
-              />
-              <Route path="/badgesmanagement" element={<BadgeManagement />} />
-              <Route path="/teamprogress" element={<TeamsProgress />} />
-            </>
-          )}
+            {userData?.Role === "Operation Manager" && (
+              <>
+                <Route path="/" element={<Navigate to="/homeom" />} />
+                <Route path="/homeom" element={<HomeOM />} />
+                <Route path="/upcampaign" element={<UpCampaign />} />
+              </>
+            )}
+            {userData?.Role === "QA Lead" && (
+              <>
+                <Route path="/" element={<Navigate to="/homeqal" />} />
+                <Route path="/upquiz" element={<UpQuiz />} />
+                <Route path="/homeqal" element={<HomeQAL />} />
+              </>
+            )}
+            {userData?.Role === "Reporting Lead" && (
+              <>
+                <Route path="/" element={<Navigate to="/homerl" />} />
+                <Route path="/homerl" element={<HomeRL />} />
+                <Route path="/upagents" element={<UpAgents />} />
+              </>
+            )}
+            {userData?.Role === "Super Admin" && (
+              <>
+                <Route path="/" element={<Navigate to="/homesa" />} />
+                <Route path="/homesa" element={<HomeSA />} />
+                <Route path="/upcount" element={<UpCount />} />
+              </>
+            )}
+            {userData?.Role === "Team Leader" && (
+              <>
+                <Route path="/" element={<Navigate to="/hometl" />} />
+                <Route path="/hometl" element={<HomeTL />} />
+                <Route path="/homeusers" element={<HomeUser />} />
+                <Route path="/followingteams" element={<FollowingTeamsKPI />} />
+                <Route
+                  path="/challengeasignment"
+                  element={<ChallengeAssignment />}
+                />
+                <Route path="/badgesmanagement" element={<BadgeManagement />} />
+                <Route path="/teamprogress" element={<TeamsProgress />} />
+              </>
+            )}
 
-          {!userData?.Role && <Route path="/" element={<Login />} />}
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
+            {!userData?.Role && <Route path="/" element={<Login />} />}
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
+        )}
       </MainApp>
     </Router>
   );
