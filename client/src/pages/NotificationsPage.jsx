83527154ce6d4b4d4980b0a14854de9ce0b43ec2
Daniel Moreno <tpdsms@gmail.com>
2022-04-04 10:39:43 -0500

    Pull deiby 4 de abril

--- client/src/pages/NotificationsPage.jsx
+++ client/src/pages/NotificationsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Typography, Grid, styled, Box } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
@@ -9,182 +9,182 @@ import Footer from "../components/Footer";
 import { getNotifications } from "../utils/api";
 import LoadingComponent from "../components/LoadingComponent";
 const MainNotification = styled(Grid)(() => ({
-  width: "100%",
-  h6: {
-    color: "#3047b0",
-    fontWeight: "500",
-    margin: "1rem 0 2rem 0",
-  },
+	width: "100%",
+	h6: {
+		color: "#3047b0",
+		fontWeight: "500",
+		margin: "1rem 0 2rem 0",
+	},
 }));
 
 const NotiBox = styled(Box)(() => ({
-  height: "60vh",
-  overflowY: "scroll",
-  backgroundColor: "#f9f9f9",
-  borderRadius: "20px",
-  padding: "1rem",
-  "&::-webkit-scrollbar": {
-    width: "6px" /* width of the entire scrollbar */,
-  },
+	height: "60vh",
+	overflowY: "scroll",
+	backgroundColor: "#f9f9f9",
+	borderRadius: "20px",
+	padding: "1rem",
+	"&::-webkit-scrollbar": {
+		width: "6px" /* width of the entire scrollbar */,
+	},
 
-  "&::-webkit-scrollbar-track": {
-    background: "white" /* color of the tracking area */,
-  },
-  "&::-webkit-scrollbar-thumb": {
-    backgroundColor: "#e8e8e8" /* color of the scroll thumb */,
-    borderRadius: "20px" /* roundness of the scroll thumb */,
-  },
+	"&::-webkit-scrollbar-track": {
+		background: "white" /* color of the tracking area */,
+	},
+	"&::-webkit-scrollbar-thumb": {
+		backgroundColor: "#e8e8e8" /* color of the scroll thumb */,
+		borderRadius: "20px" /* roundness of the scroll thumb */,
+	},
 }));
 
 const NotificationsPage = ({ count }) => {
-  const userData = useSelector((store) => store.loginUser.userData);
-  const idccms = userData.Idccms;
+	const userData = useSelector((store) => store.loginUser.userData);
+	const idccms = userData.Idccms;
 
-  const [notificationUser, setNotificationUser] = useState([]);
-  const [notificationTeam, setNotificationTeam] = useState([]);
-  const [skipUser, setSkipUser] = useState(0);
-  const [limitUser, setLimitUser] = useState(9);
-  const [skipTeam, setSkipTeam] = useState(0);
-  const [limitTeam, setLimitTeam] = useState(9);
-  const [pageUser, setPageUser] = useState(1);
-  const [pageTeam, setPageTeam] = useState(1);
-  const [countUser, setCountUser] = useState(0);
-  const [countTeam, setCountTeam] = useState(10);
-  const [loading, setLoading] = useState(false);
-  const [loading2, setLoading2] = useState(false);
+	const [notificationUser, setNotificationUser] = useState([]);
+	const [notificationTeam, setNotificationTeam] = useState([]);
+	const [skipUser, setSkipUser] = useState(0);
+	const [limitUser, setLimitUser] = useState(9);
+	const [skipTeam, setSkipTeam] = useState(0);
+	const [limitTeam, setLimitTeam] = useState(9);
+	const [pageUser, setPageUser] = useState(1);
+	const [pageTeam, setPageTeam] = useState(1);
+	const [countUser, setCountUser] = useState(0);
+	const [countTeam, setCountTeam] = useState(10);
+	const [loading, setLoading] = useState(false);
+	const [loading2, setLoading2] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    const traerNotificaciones = async () => {
-      const data = await getNotifications(idccms, skipUser, limitUser, 1);
-      setNotificationUser(data.data);
-      if(data.data.length > 0){
-        const pages =data.data[0].NotificationRead + data.data[0].NotificationUnread;
-        setCountUser(parseInt(pages / 10 + 1));
-      }else{
-        setCountUser(1)
-      }
-      setLoading(false);
-    };
-    traerNotificaciones();
-    // eslint-disable-next-line
-  }, [limitUser]);
+	useEffect(() => {
+		setLoading(true);
+		const traerNotificaciones = async () => {
+			const data = await getNotifications(idccms, skipUser, limitUser, 1);
+			setNotificationUser(data.data);
+			if (data.data.length > 0) {
+				const pages =
+					data.data[0].NotificationRead + data.data[0].NotificationUnread;
+				setCountUser(parseInt(pages / 10 + 1));
+			} else {
+				setCountUser(1);
+			}
+			setLoading(false);
+		};
+		traerNotificaciones();
+		// eslint-disable-next-line
+	}, [limitUser]);
 
-  useEffect(() => {
-    setLoading2(true);
-    const traerNotificaciones = async () => {
-      const dataTeam = await getNotifications(idccms, skipTeam, limitTeam, 2);
-      setNotificationTeam(dataTeam.data);
+	useEffect(() => {
+		setLoading2(true);
+		const traerNotificaciones = async () => {
+			const dataTeam = await getNotifications(idccms, skipTeam, limitTeam, 2);
+			setNotificationTeam(dataTeam.data);
 
-        if(dataTeam.data.length > 0){
-        const pagesTeam =
-          dataTeam.data[0].NotificationRead + dataTeam.data[0].NotificationUnread;
-        setCountTeam(parseInt(pagesTeam / 10 + 1));
-      }else{
-        setCountTeam(1)
-      }
-      setLoading2(false);
-    };
-    traerNotificaciones();
-    // eslint-disable-next-line
-  }, [limitTeam]);
+			if (dataTeam.data.length > 0) {
+				const pagesTeam =
+					dataTeam.data[0].NotificationRead +
+					dataTeam.data[0].NotificationUnread;
+				setCountTeam(parseInt(pagesTeam / 10 + 1));
+			} else {
+				setCountTeam(1);
+			}
+			setLoading2(false);
+		};
+		traerNotificaciones();
+		// eslint-disable-next-line
+	}, [limitTeam]);
 
-  const paginationUser = (e, value) => {
-    setPageUser(value);
-    if (value !== 1) {
-      setSkipUser(() => (value - 1) * 10);
-      setLimitUser(() => value * 10);
-    } else {
-      setSkipUser(() => 0);
-      setLimitUser(() => 9);
-    }
-  };
-  const paginationTeam = (e, value) => {
-    setPageTeam(value);
-    if (value !== 1) {
-      setSkipTeam(() => (value - 1) * 10);
-      setLimitTeam(() => value * 10);
-    } else {
-      setSkipTeam(() => 0);
-      setLimitTeam(() => 9);
-    }
-  };
+	const paginationUser = (e, value) => {
+		setPageUser(value);
+		if (value !== 1) {
+			setSkipUser(() => (value - 1) * 10);
+			setLimitUser(() => value * 10);
+		} else {
+			setSkipUser(() => 0);
+			setLimitUser(() => 9);
+		}
+	};
+	const paginationTeam = (e, value) => {
+		setPageTeam(value);
+		if (value !== 1) {
+			setSkipTeam(() => (value - 1) * 10);
+			setLimitTeam(() => value * 10);
+		} else {
+			setSkipTeam(() => 0);
+			setLimitTeam(() => 9);
+		}
+	};
 
+	return (
+		<MainNotification>
+			<Header count={count} />
+			<Grid container spacing={3}>
+				<Grid item xs={12} md={6}>
+					<Typography variant="h6" color="initial">
+						Me Notifications
+					</Typography>
+					<NotiBox>
+						{loading ? (
+							<LoadingComponent />
+						) : (
+							<>
+								{notificationUser.length > 0 ? (
+									notificationUser.map((note, index) => (
+										<NotificationCard info={note} key={index} />
+									))
+								) : (
+									<Typography variant="h6" color="initial">
+										No Notifications
+									</Typography>
+								)}
+							</>
+						)}
+					</NotiBox>
+					<Stack spacing={2}>
+						<Pagination
+							count={countUser}
+							color="primary"
+							size="large"
+							onChange={paginationUser}
+							page={pageUser}
+						/>
+					</Stack>
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<Typography variant="h6" color="initial">
+						Team Notifications
+					</Typography>
 
-  console.log(notificationUser.length)
-  return (
-    <MainNotification>
-      <Header count={count} />
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6" color="initial">
-            Me Notifications
-          </Typography>
-          <NotiBox>
-            {loading ? (
-              <LoadingComponent />
-            ) : (
-              <>
-                {
-                  notificationUser.length > 0 ?
-                  notificationUser.map((note, index) => (
-                  <NotificationCard info={note} key={index} />
-                )) :
-                 <Typography variant="h6" color="initial">
-            No Notifications
-          </Typography>
-                }
-              </>
-            )}
-          </NotiBox>
-          <Stack spacing={2}>
-            <Pagination
-              count={countUser}
-              color="primary"
-              size="large"
-              onChange={paginationUser}
-              page={pageUser}
-            />
-          </Stack>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6" color="initial">
-            Team Notifications
-          </Typography>
-
-          <NotiBox>
-            {loading2 ? (
-              <LoadingComponent />
-            ) : (
-              <>
-{
-  notificationTeam.length >0 ?
-              notificationTeam.map((note, index) => (
-                <NotificationCard info={note} key={index} />
-              )) :
-               <Typography variant="h6" color="initial">
-            No Notifications
-          </Typography>
-}
-              </>
-            )}
-          </NotiBox>
-          <Box display="flex" justifyContent="right">
-            <Stack spacing={2}>
-              <Pagination
-                count={countTeam}
-                color="primary"
-                size="large"
-                onChange={paginationTeam}
-                page={pageTeam}
-              />
-            </Stack>
-          </Box>
-        </Grid>
-      </Grid>
-      <Footer />
-    </MainNotification>
-  );
+					<NotiBox>
+						{loading2 ? (
+							<LoadingComponent />
+						) : (
+							<>
+								{notificationTeam.length > 0 ? (
+									notificationTeam.map((note, index) => (
+										<NotificationCard info={note} key={index} />
+									))
+								) : (
+									<Typography variant="h6" color="initial">
+										No Notifications
+									</Typography>
+								)}
+							</>
+						)}
+					</NotiBox>
+					<Box display="flex" justifyContent="right">
+						<Stack spacing={2}>
+							<Pagination
+								count={countTeam}
+								color="primary"
+								size="large"
+								onChange={paginationTeam}
+								page={pageTeam}
+							/>
+						</Stack>
+					</Box>
+				</Grid>
+			</Grid>
+			<Footer />
+		</MainNotification>
+	);
 };
 
 export default NotificationsPage;
--- client/src/pages/TeamLeader/TLChallengeAssignment.jsx
+++ client/src/pages/TeamLeader/TLChallengeAssignment.jsx
@@ -4,9 +4,9 @@ import { Grid, styled, Typography, Box } from "@mui/material";
 import Header from "../../components/homeUser/Header";
 import Footer from "../../components/Footer";
 import {
-  downloadActivities,
-  downloadUsers,
-  assingChallenges,
+	downloadActivities,
+	downloadUsers,
+	assingChallenges,
 } from "../../utils/api";
 import LoadingComponent from "../../components/LoadingComponent";
 import { ModalLoading } from "../../components/ModalLoading";
@@ -22,206 +22,206 @@ import FormCreateNewChallenge from "../../components/Modals/FormCreateNewChallen
 const MySwal = withReactContent(Swal);
 
 const BoxSelectBadge = styled(Grid)(() => ({
-  button: {
-    textTransform: "none",
-    background: "#fff",
-    margin: "5px",
-    width: "9rem",
-    fontWeight: "600",
-    border: "1px solid #00000009",
-  },
+	button: {
+		textTransform: "none",
+		background: "#fff",
+		margin: "5px",
+		width: "9rem",
+		fontWeight: "600",
+		border: "1px solid #00000009",
+	},
 
-  margin: "2rem 0",
+	margin: "2rem 0",
 }));
 
 const BoxActivity = styled(Grid)(() => ({
-  background: "#f2f2f2",
-  padding: "1rem",
-  borderRadius: "20px",
+	background: "#f2f2f2",
+	padding: "1rem",
+	borderRadius: "20px",
 }));
 
 const Boxview = styled(Grid)(() => ({
-  overflowY: "scroll",
-  height: "50vh",
-  "&::-webkit-scrollbar": {
-    width: "6px",
-  },
+	overflowY: "scroll",
+	height: "50vh",
+	"&::-webkit-scrollbar": {
+		width: "6px",
+	},
 
-  "&::-webkit-scrollbar-track": {
-    background: "white",
-  },
-  "&::-webkit-scrollbar-thumb": {
-    backgroundColor: "#e8e8e8",
-    borderRadius: "20px",
-  },
+	"&::-webkit-scrollbar-track": {
+		background: "white",
+	},
+	"&::-webkit-scrollbar-thumb": {
+		backgroundColor: "#e8e8e8",
+		borderRadius: "20px",
+	},
 }));
 
 export const TLChallengeAssignment = ({ count }) => {
-  const [loading, setLoading] = useState(false);
-  const [fullLoading, setFullLoading] = useState(false);
-  const userData = useSelector((store) => store.loginUser.userData);
-  const idccms = userData.Idccms;
-  const userName = userData.Nombre;
-  const [activity, setActivity] = useState([]);
-  const [error, setError] = useState(false);
-  const [users, setUsers] = useState([]);
-  const [openModal, setOpenModal] = useState(false);
-  const handleOpen = () => setOpenModal(true);
-  const handleClose = () => setOpenModal(false);
-  useEffect(() => {
-    const getData = async () => {
-      const user = await downloadUsers(idccms);
-      if (user && user.status === 200 && user.data.length > 1) {
-        setLoading(true);
-        let ccmsAgent = user.data[0].Agents[0].ident;
-        user.data[0].Agents[0].isChecked = true;
-        setUsers(user.data[0].Agents);
-        const activities = await downloadActivities(ccmsAgent, idccms);
-        if (
-          activities &&
-          activities.status === 200 &&
-          activities.data.length > 1
-        ) {
-          setActivity(activities.data);
-          setLoading(false);
-        } else {
-          setError(true);
-        }
-      } else {
-        setError(true);
-      }
-    };
+	const [loading, setLoading] = useState(false);
+	const [fullLoading, setFullLoading] = useState(false);
+	const userData = useSelector((store) => store.loginUser.userData);
+	const idccms = userData.Idccms;
+	const userName = userData.Nombre;
+	const [activity, setActivity] = useState([]);
+	const [error, setError] = useState(false);
+	const [users, setUsers] = useState([]);
+	const [openModal, setOpenModal] = useState(false);
+	const handleOpen = () => setOpenModal(true);
+	const handleClose = () => setOpenModal(false);
+	useEffect(() => {
+		const getData = async () => {
+			const user = await downloadUsers(idccms);
+			if (user && user.status === 200 && user.data.length > 1) {
+				setLoading(true);
+				let ccmsAgent = user.data[0].Agents[0].ident;
+				user.data[0].Agents[0].isChecked = true;
+				setUsers(user.data[0].Agents);
+				const activities = await downloadActivities(ccmsAgent, idccms);
+				if (
+					activities &&
+					activities.status === 200 &&
+					activities.data.length > 1
+				) {
+					setActivity(activities.data);
+					setLoading(false);
+				} else {
+					setError(true);
+				}
+			} else {
+				setError(true);
+			}
+		};
 
-    getData();
-    // eslint-disable-next-line
-  }, []);
+		getData();
+		// eslint-disable-next-line
+	}, []);
 
-  //funcion de asingacion de usuarios
-  const handleUser = async (e) => {
-    setLoading(true);
-    const { value, checked } = e.target;
-    let tempUser = users.map((user) =>
-      user.Agent === value.split("-")[0]
-        ? { ...user, isChecked: checked }
-        : { ...user, isChecked: false }
-    );
-    setUsers(tempUser);
-    const activities = await downloadActivities(value.split("-")[1], idccms);
-    if (activities && activities.status === 200 && activities.data.length > 1) {
-      setActivity(activities.data);
-      setLoading(false);
-    } else {
-      setError(true);
-    }
-  };
+	//funcion de asingacion de usuarios
+	const handleUser = async (e) => {
+		setLoading(true);
+		const { value, checked } = e.target;
+		let tempUser = users.map((user) =>
+			user.Agent === value.split("-")[0]
+				? { ...user, isChecked: checked }
+				: { ...user, isChecked: false }
+		);
+		setUsers(tempUser);
+		const activities = await downloadActivities(value.split("-")[1], idccms);
+		if (activities && activities.status === 200 && activities.data.length > 1) {
+			setActivity(activities.data);
+			setLoading(false);
+		} else {
+			setError(true);
+		}
+	};
 
-  ///Función Envio de Challenges
-  const handleSubmit = async (data) => {
-    setFullLoading(true);
-    const dataToSendChallenge = await validateDataCheck(users, data, userName);
-    const sendChallenge = await assingChallenges(
-      dataToSendChallenge[0],
-      idccms
-    );
-    if (sendChallenge && sendChallenge.status === 200) {
-      setFullLoading(false);
-      MySwal.fire({
-        title: <p>Successful Assignments</p>,
-        icon: "success",
-        confirmButtonText: "Accept",
-        allowOutsideClick: false,
-      }).then((resultado) => {
-        if (resultado.value) {
-          window.location.reload();
-        }
-      });
-    } else {
-      setLoading(false);
-      MySwal.fire({
-        title: <p>Send Error</p>,
-        icon: "error",
-      });
-    }
-  };
-  return (
-    <>
-      {fullLoading && <ModalLoading />}
-      <FormCreateNewChallenge openModal={openModal} handleClose={handleClose} />
+	///Función Envio de Challenges
+	const handleSubmit = async (data) => {
+		setFullLoading(true);
+		const dataToSendChallenge = await validateDataCheck(users, data, userName);
+		const sendChallenge = await assingChallenges(
+			dataToSendChallenge[0],
+			idccms
+		);
+		if (sendChallenge && sendChallenge.status === 200) {
+			setFullLoading(false);
+			MySwal.fire({
+				title: <p>Successful Assignments</p>,
+				icon: "success",
+				confirmButtonText: "Accept",
+				allowOutsideClick: false,
+			}).then((resultado) => {
+				if (resultado.value) {
+					window.location.reload();
+				}
+			});
+		} else {
+			setLoading(false);
+			MySwal.fire({
+				title: <p>Send Error</p>,
+				icon: "error",
+			});
+		}
+	};
+	return (
+		<>
+			{fullLoading && <ModalLoading />}
+			<FormCreateNewChallenge openModal={openModal} handleClose={handleClose} />
 
-      <MainPage>
-        <Header count={count} />
-        <Grid container>
-          <Grid item xs={6}>
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Typography variant="h5" fontWeight={500}>
-                Challenge Assignment
-              </Typography>
+			<MainPage>
+				<Header count={count} />
+				<Grid container>
+					<Grid item xs={6}>
+						<Box
+							display="flex"
+							justifyContent="space-between"
+							alignItems="center"
+						>
+							<Typography variant="h5" fontWeight={500}>
+								Challenge Assignment
+							</Typography>
 
-              <ButtonAction onClick={handleOpen}>
-                Create New Challenge
-              </ButtonAction>
-            </Box>
-          </Grid>
+							<ButtonAction onClick={handleOpen}>
+								Create New Challenge
+							</ButtonAction>
+						</Box>
+					</Grid>
 
-          <BoxSelectBadge item xs={4}></BoxSelectBadge>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6} padding={1}>
-            <BoxActivity>
-              <Box marginBottom={2}></Box>
-              <Boxview>
-                {!error ? (
-                  users.map((user, index) => (
-                    <UserChallenge
-                      key={index}
-                      user={user}
-                      handleUser={handleUser}
-                    />
-                  ))
-                ) : (
-                  <Typography variant="h5" fontWeight={500}>
-                    The Game Starts Soon
-                  </Typography>
-                )}
-              </Boxview>
-            </BoxActivity>
-          </Grid>
-          <Grid item xs={12} md={6} padding={1}>
-            <BoxActivity>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                marginBottom={2}
-              ></Box>
-              <Boxview>
-                {!error ? (
-                  loading ? (
-                    <LoadingComponent />
-                  ) : (
-                    activity?.map((act, index) => (
-                      <ChallengeCard
-                        key={index}
-                        data={act}
-                        handleSubmit={handleSubmit}
-                      />
-                    ))
-                  )
-                ) : (
-                  <Typography variant="h5" fontWeight={500}>
-                    The Game Starts Soon
-                  </Typography>
-                )}
-              </Boxview>
-            </BoxActivity>
-          </Grid>
-        </Grid>
-        <Footer />
-      </MainPage>
-    </>
-  );
+					<BoxSelectBadge item xs={4}></BoxSelectBadge>
+				</Grid>
+				<Grid container spacing={2}>
+					<Grid item xs={12} md={6} padding={1}>
+						<BoxActivity>
+							<Box marginBottom={2}></Box>
+							<Boxview>
+								{!error ? (
+									users.map((user, index) => (
+										<UserChallenge
+											key={index}
+											user={user}
+											handleUser={handleUser}
+										/>
+									))
+								) : (
+									<Typography variant="h5" fontWeight={500}>
+										The Game Starts Soon
+									</Typography>
+								)}
+							</Boxview>
+						</BoxActivity>
+					</Grid>
+					<Grid item xs={12} md={6} padding={1}>
+						<BoxActivity>
+							<Box
+								display="flex"
+								alignItems="center"
+								justifyContent="space-between"
+								marginBottom={2}
+							></Box>
+							<Boxview>
+								{!error ? (
+									loading ? (
+										<LoadingComponent />
+									) : (
+										activity?.map((act, index) => (
+											<ChallengeCard
+												key={index}
+												data={act}
+												handleSubmit={handleSubmit}
+											/>
+										))
+									)
+								) : (
+									<Typography variant="h5" fontWeight={500}>
+										The Game Starts Soon
+									</Typography>
+								)}
+							</Boxview>
+						</BoxActivity>
+					</Grid>
+				</Grid>
+				<Footer />
+			</MainPage>
+		</>
+	);
 };
