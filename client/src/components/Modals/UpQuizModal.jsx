d1260d7b59cb33d7fcf73d49b56c5c19cf4141eb
Daniel Moreno <tpdsms@gmail.com>
2022-04-04 12:16:03 -0500

    Instructions load KPI

--- client/src/components/Modals/UpQuizModal.jsx
+++ client/src/components/Modals/UpQuizModal.jsx
@@ -5,150 +5,184 @@ import Quiz_template from "../../assets/filesTemplatesCSV/QuizTemplate.csv";
 import OM_template from "../../assets/filesTemplatesCSV/OpsManagerTemplate.csv";
 import RL_template from "../../assets/filesTemplatesCSV/ReportingTemplate.csv";
 import SU_template from "../../assets/filesTemplatesCSV/SuperUserTemplate.csv";
+import Kpi_template from "../../assets/filesTemplatesCSV/Load_Kpi_Template.csv";
 
 const MainModal = styled(Box)(() => ({
-  minHeight: "50vh",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-around",
-  textAlign: "center",
+	minHeight: "50vh",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "space-around",
+	textAlign: "center",
 
-  h5: {
-    fontWeight: 900,
-    marginBottom: "2rem",
-  },
+	h5: {
+		fontWeight: 900,
+		marginBottom: "2rem",
+	},
 
-  button: {
-    textTransform: "none",
-    color: "white",
-    width: "177px",
-    height: "61px",
-    background: "linear-gradient(180deg, #3047B0 0%, #0087FF 100%)",
-  },
+	button: {
+		textTransform: "none",
+		color: "white",
+		width: "177px",
+		height: "61px",
+		background: "linear-gradient(180deg, #3047B0 0%, #0087FF 100%)",
+	},
 }));
 const Su = () => {
-  return (
-    <Typography variant="body2" color="initial">
-      Ident: CCMSID del operation Manager (Solo números)
-      <br /> TeamName: Nombre del equipo <br /> KPI: KPI a evaluar (i.e.: %Abs,
-      AHT_In, QACL )<br /> Campaign: Nombre de la campaña
-      <br /> Q1: valores objetivos para agentes Q1 (solo números)
-      <br /> Q2: valores objetivos para agentes Q2 (solo números) <br /> Q3:
-      valores objetivos para agentes Q3 (solo números) <br /> Q4: valores
-      objetivos para agentes Q4 (solo números)
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="initial">
+			Ident: CCMSID del operation Manager (Solo números)
+			<br /> TeamName: Nombre del equipo <br /> KPI: KPI a evaluar (i.e.: %Abs,
+			AHT_In, QACL )<br /> Campaign: Nombre de la campaña
+			<br /> Q1: valores objetivos para agentes Q1 (solo números)
+			<br /> Q2: valores objetivos para agentes Q2 (solo números) <br /> Q3:
+			valores objetivos para agentes Q3 (solo números) <br /> Q4: valores
+			objetivos para agentes Q4 (solo números)
+			<br />
+			Target: valor objetivo para el KPI (solo números)
+			<br />
+			Order: "asc"(mas mejor) o "dsc"(menos mejor)
+		</Typography>
+	);
 };
 const Om = () => {
-  return (
-    <Typography variant="body2" color="initial">
-      Ident: CCMSID del usuario al cual se le va asignar un rol (Solo números)
-      <br />
-      RoleAgent: Rol que va a desempeñar el usuario. (Team Leader, Reporting
-      Lead,QA Lead) <br />
-      Team: Nombre del equipo al cual va a pertenecer
-      <br /> Lob: Nombre de la lobbie
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="initial">
+			Ident: CCMSID del usuario al cual se le va asignar un rol (Solo números)
+			<br />
+			RoleAgent: Rol que va a desempeñar el usuario. (Team Leader, Reporting
+			Lead,QA Lead) <br />
+			Team: Nombre del equipo al cual va a pertenecer
+			<br /> Lob: Nombre de la "line of bussines"
+		</Typography>
+	);
 };
 const Rep = () => {
-  return (
-    <Typography variant="body2" color="initial">
-      Quartile: Cuartile en que se ubica el agente (Q1, Q2, Q3, Q4). <br />
-      Ident: CCMSID del agente (solo numeros)
-      <br /> Team: Nombre del equipo al que va a pertenecer
-      <br /> RoleAgent: en este campo solo debe ir la palabra Agent
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="initial">
+			Quartile: Cuartile en que se ubica el agente (Q1, Q2, Q3, Q4). <br />
+			Ident: CCMSID del agente (solo numeros)
+			<br /> Team: Nombre del equipo al que va a pertenecer
+			<br /> RoleAgent: en este campo solo debe ir la palabra "Agent"
+		</Typography>
+	);
 };
 
 const Qa = () => {
-  return (
-    <Typography variant="body2" color="initial">
-      Question: campo para ingresar el enunciado de la pregunta
-      <br /> Option1: primera respuesta
-      <br /> Option2: segunda respuesta <br />
-      Option3: Tercera respuesta (dejar en blanco, si la pregunta es verdaro o
-      falso) <br />
-      Option4: Cuarta respuesta (dejar en blanco, si la pregunta es verdaro o
-      falso)
-      <br /> Answer: Respuesta correcta (debe ser el mismo texto de la opcion)
-      <br />
-      Quartile: Cuartil del agente al que va destinada la pregunta
-      <br /> ExamName: Nombre del examen DescriptionExam: Descripcion del examen
-      <br />
-      <br /> ExamName: Nombre del examen
-      <br /> DescriptionExam: Descripcion del examen
-      <br />
-      ApprovalExam: Valor en porcentaje para aprobar el examen (solo numeros)
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="initial">
+			Question: campo para ingresar el enunciado de la pregunta
+			<br /> Option1: primera respuesta
+			<br /> Option2: segunda respuesta <br />
+			Option3: Tercera respuesta (dejar en blanco, si la pregunta es verdaro o
+			falso) <br />
+			Option4: Cuarta respuesta (dejar en blanco, si la pregunta es verdaro o
+			falso)
+			<br /> Answer: Respuesta correcta (debe ser el mismo texto de la opcion)
+			<br />
+			Quartile: Cuartil del agente al que va destinada la pregunta
+			<br /> ExamName: Nombre del examen
+			<br /> DescriptionExam: Descripcion del examen
+			<br /> ApprovalExam: Porcentaje con el que se aprueba el examen (solo
+			numeros sin el signo %)
+			<br /> LOB: Nombre de la "line of bussines"
+			<br />
+			Topic: Nombre de la categoria a la que pertenece el quiz
+		</Typography>
+	);
 };
+
+const LoadKpis = () => {
+	return (
+		<Typography variant="body2" color="initial">
+			Kpi: campo para ingresar el acronimo del KPI
+			<br /> unitKpi: Unidad de medida del Kpi(Percentage, Seconds, Minutes,
+			Avg)
+			<br /> Type: Tipo de Unidad (1,2,4,5) <br />
+			<br />
+			La relacion entre Type y unitKpi debe ser
+			<br /> 1= Hours
+			<br /> 2 = Seconds
+			<br />4 = Avg
+			<br />5 = Percentage
+			<br />
+			<br /> Idccms:CCMSID del agente (solo numeros)
+			<br /> Date: Fecha de resultado de Kpi (sigue el siguiente formato
+			MM/DD/YYYY)
+			<br /> Score: Valor de resultado Kpi (solo numeros)
+		</Typography>
+	);
+};
+
 const UpQuizModal = ({ handleClose, template }) => {
-  let dwounloadTemplate;
-  switch (template) {
-    case "Quiz Template":
-      dwounloadTemplate = Quiz_template;
-      break;
-    case "OM Template":
-      dwounloadTemplate = OM_template;
-      break;
-    case "Rep Lead Template":
-      dwounloadTemplate = RL_template;
-      break;
-    case "Super User Template":
-      dwounloadTemplate = SU_template;
-      break;
+	let dwounloadTemplate;
+	switch (template) {
+		case "Quiz Template":
+			dwounloadTemplate = Quiz_template;
+			break;
+		case "OM Template":
+			dwounloadTemplate = OM_template;
+			break;
+		case "Rep Lead Template":
+			dwounloadTemplate = RL_template;
+			break;
+		case "Super User Template":
+			dwounloadTemplate = SU_template;
+			break;
+		case "Load Kpi Template":
+			dwounloadTemplate = Kpi_template;
+			break;
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 
-  //Funcion para descargar archivos locales
-  const downloadFile = () => {
-    var link = document.createElement("a");
-    link.setAttribute("download", template);
-    //link.href = Quiz_template;
-    link.href = dwounloadTemplate;
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-    handleClose();
-  };
+	//Funcion para descargar archivos locales
+	const downloadFile = () => {
+		var link = document.createElement("a");
+		link.setAttribute("download", template);
+		//link.href = Quiz_template;
+		link.href = dwounloadTemplate;
+		document.body.appendChild(link);
+		link.click();
+		link.remove();
+		handleClose();
+	};
 
-  return (
-    <MainModal>
-      <img src={instructions} alt="instructions" />
-      <Box marginBottom={5}>
-        <Typography variant="h5" color="initial">
-          Instructions for downloading the {template}
-        </Typography>
-        <Typography variant="body1" color="initial">
-          Below you will find the instructions that you must attend and carry
-          out to download the {template}
-        </Typography>
-        {template === "Quiz Template" ? (
-          <Qa />
-        ) : template === "OM Template" ? (
-          <Om />
-        ) : template === "Rep Lead Template" ? (
-          <Rep />
-        ) : template === "Super User Template" ? (
-          <Su />
-        ) : (
-          <Typography variant="body2" color="initial">
-            We work
-          </Typography>
-        )}
-      </Box>
-      <Box>
-        <Button sx={{ marginRight: "2rem" }} onClick={handleClose}>
-          Return
-        </Button>
-        <Button onClick={() => downloadFile()}>Download</Button>
-      </Box>
-    </MainModal>
-  );
+	return (
+		<MainModal>
+			<img src={instructions} alt="instructions" />
+			<Box marginBottom={5}>
+				<Typography variant="h5" color="initial">
+					Instructions for downloading the {template}
+				</Typography>
+				<Typography variant="body1" color="initial">
+					Below you will find the instructions that you must attend and carry
+					out to download the {template}
+				</Typography>
+				{template === "Quiz Template" ? (
+					<Qa />
+				) : template === "OM Template" ? (
+					<Om />
+				) : template === "Rep Lead Template" ? (
+					<Rep />
+				) : template === "Super User Template" ? (
+					<Su />
+				) : template === "Load Kpi Template" ? (
+					<LoadKpis />
+				) : (
+					<Typography variant="body2" color="initial">
+						We work
+					</Typography>
+				)}
+			</Box>
+			<Box>
+				<Button sx={{ marginRight: "2rem" }} onClick={handleClose}>
+					Return
+				</Button>
+				<Button onClick={() => downloadFile()}>Download</Button>
+			</Box>
+		</MainModal>
+	);
 };
 
 export default UpQuizModal;
--- client/src/pages/Rep Lead/KpiUpload.jsx
+++ client/src/pages/Rep Lead/KpiUpload.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  MainPage,
-  BoxUpFile,
-  ButtonAction,
+	MainPage,
+	BoxUpFile,
+	ButtonAction,
 } from "../../assets/styled/muistyled";
 import Footer from "../../components/Footer";
 import Header from "../../components/homeUser/Header";
-import { Grid, Typography, styled, Box } from "@mui/material";
+import { Grid, Typography, styled, Box, Modal } from "@mui/material";
 import { FiDownload, FiUpload } from "react-icons/fi";
 import TableKPIUpload from "../../components/ReportingLead/TableKPIUpload";
 import downloadTemplate from "../../assets/filesTemplatesCSV/Load_Kpi_Template.csv";
@@ -16,208 +16,222 @@ import { useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import {
-  validateFieldsUploadKPIs,
-  validateHeadersUploadKPIs,
+	validateFieldsUploadKPIs,
+	validateHeadersUploadKPIs,
 } from "../../helpers/helpers";
 import { getKPIsCampaign, uploadKPIs } from "../../utils/api";
+import UpQuizModal from "../../components/Modals/UpQuizModal";
 
 const MySwal = withReactContent(Swal);
 
 const Item = styled(Box)(() => ({
-  background: "#f9f9f9",
-  borderRadius: "10px",
-  p: {
-    color: "#3047B0",
-  },
+	background: "#f9f9f9",
+	borderRadius: "10px",
+	p: {
+		color: "#3047B0",
+	},
 }));
 
 const BoxButton = styled(Box)(() => ({
-  display: "flex",
-  margin: "1rem 0",
+	display: "flex",
+	margin: "1rem 0",
 }));
 
-/* const dataKPI = [
-  { acron: "AHT", description: "Average Handle Time", idKPI: 12 },
-  { acron: "SSKI", description: "Soft Skills", idKPI: 23 },
-  { acron: "QA", description: "Quality Assurance", idKPI: 34 },
-  { acron: "EXE", description: "Execution", idKPI: 56 },
-  { acron: "ABS", description: "Absemteeism", idKPI: 67 },
-]; */
+const ModalBox = styled(Box)(() => ({
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	//width: 400,
+	borderRadius: "20px",
+	boxShadow: "2px 2px 5px #2f2f2f",
+	padding: "1rem",
+	backgroundColor: "RGBA(255,255,255,0.9)",
+}));
 
 const KpiUpload = () => {
-  const userData = useSelector((store) => store.loginUser.userData);
-  const idccms = userData.Idccms;
-  const [loading, setLoading] = useState(false);
-  const [tableLoading, setTableLoading] = useState(false);
-  const [dataKpi, setDataKpi] = useState([]);
+	const userData = useSelector((store) => store.loginUser.userData);
+	const idccms = userData.Idccms;
+	const [loading, setLoading] = useState(false);
+	const [tableLoading, setTableLoading] = useState(false);
+	const [dataKpi, setDataKpi] = useState([]);
+	const [template, setTemplate] = useState("");
+	const [open, setOpen] = React.useState(false);
 
-  useEffect(() => {
-    const getdata = async () => {
-      setTableLoading(true);
-      const kpis = await getKPIsCampaign(idccms);
-      setTableLoading(false);
-      setDataKpi(kpis.data);
-    };
-    getdata();
-    // eslint-disable-next-line
-  }, []);
+	useEffect(() => {
+		const getdata = async () => {
+			setTableLoading(true);
+			const kpis = await getKPIsCampaign(idccms);
+			setTableLoading(false);
+			setDataKpi(kpis.data);
+		};
+		getdata();
+		// eslint-disable-next-line
+	}, []);
+	const handleOpen = () => {
+		setOpen(true);
+		setTemplate("Load Kpi Template");
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  const loadFile = (e) => {
-    setLoading(true);
-    const fileCSV = e.target.files[0];
+	const loadFile = (e) => {
+		setLoading(true);
+		const fileCSV = e.target.files[0];
 
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        //Parse data
-        const ab = e.target.result;
-        const wb = XLSX.readFile(ab, {
-          type: "array",
-          cellDates: true,
-          cellText: false,
-          cellNF: false,
-        });
-        //Get first worksheet
-        const wsname = wb.SheetNames[0];
-        const ws = wb.Sheets[wsname];
-        //Convert array of arrays
-        const data = XLSX.utils
-          .sheet_to_json(ws, {
-            header: 1,
-            defval: "",
-            blankrows: true,
-            raw: true,
-            //dateNF: "yyyy-mm-dd",
-          })
-          .map((colum) => {
-            console.log(colum[4]);
-            return [
-              colum[0]?.toString(),
-              colum[1]?.toString(),
-              isNaN(parseInt(colum[2])) ? colum[2] : parseFloat(colum[2]),
-              isNaN(parseInt(colum[3])) ? colum[3] : parseFloat(colum[3]),
-              colum[4] === "Date"
-                ? colum[4]
-                : `${colum[4].getFullYear()}-${
-                    colum[4].getMonth() + 1
-                  }-${colum[4].getDate()}`,
-              isNaN(parseInt(colum[5])) ? colum[5] : parseFloat(colum[5]),
-            ];
-          });
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				//Parse data
+				const ab = e.target.result;
+				const wb = XLSX.readFile(ab, {
+					type: "array",
+					cellDates: true,
+					cellText: false,
+					cellNF: false,
+				});
+				//Get first worksheet
+				const wsname = wb.SheetNames[0];
+				const ws = wb.Sheets[wsname];
+				//Convert array of arrays
+				const data = XLSX.utils
+					.sheet_to_json(ws, {
+						header: 1,
+						defval: "",
+						blankrows: true,
+						raw: true,
+						//dateNF: "yyyy-mm-dd",
+					})
+					.map((colum) => {
+						console.log(colum[4]);
+						return [
+							colum[0]?.toString(),
+							colum[1]?.toString(),
+							isNaN(parseInt(colum[2])) ? colum[2] : parseFloat(colum[2]),
+							isNaN(parseInt(colum[3])) ? colum[3] : parseFloat(colum[3]),
+							colum[4] === "Date"
+								? colum[4]
+								: `${colum[4].getFullYear()}-${
+										colum[4].getMonth() + 1
+								  }-${colum[4].getDate()}`,
+							isNaN(parseInt(colum[5])) ? colum[5] : parseFloat(colum[5]),
+						];
+					});
 
-        if (data.length > 1) {
-          let differentsHeaders = validateHeadersUploadKPIs(data[0]);
+				if (data.length > 1) {
+					let differentsHeaders = validateHeadersUploadKPIs(data[0]);
 
-          if (differentsHeaders) {
-            reject(" Wrong Headers!");
-            return;
-          }
+					if (differentsHeaders) {
+						reject(" Wrong Headers!");
+						return;
+					}
 
-          data.shift();
-          let incorrectValues = validateFieldsUploadKPIs(data);
+					data.shift();
+					let incorrectValues = validateFieldsUploadKPIs(data);
 
-          if (incorrectValues) {
-            reject(" Wrong values!");
-            return;
-          }
-          resolve(data);
-        } else {
-          reject("No data!");
-        }
-        //Update state
-      };
-      reader.readAsArrayBuffer(fileCSV);
-    });
-  };
+					if (incorrectValues) {
+						reject(" Wrong values!");
+						return;
+					}
+					resolve(data);
+				} else {
+					reject("No data!");
+				}
+				//Update state
+			};
+			reader.readAsArrayBuffer(fileCSV);
+		});
+	};
 
-  const uploadFile = async (e) => {
-    const fileCSV = e.target.files[0];
-    let data;
-    if (
-      fileCSV === undefined ||
-      (fileCSV.type !== "text/csv" &&
-        fileCSV.type !== "application/vnd.ms-excel")
-    ) {
-      setLoading(false);
-      MySwal.fire({
-        title: <p>Only files in .csv format</p>,
-        icon: "error",
-      });
-    } else {
-      try {
-        data = await loadFile(e);
-        e.target.value = null;
-      } catch (error) {
-        setLoading(false);
-        MySwal.fire({
-          title: <p> {error} </p>,
-          icon: "error",
-        });
-        e.target.value = null;
-        return;
-      }
+	const uploadFile = async (e) => {
+		const fileCSV = e.target.files[0];
+		let data;
+		if (
+			fileCSV === undefined ||
+			(fileCSV.type !== "text/csv" &&
+				fileCSV.type !== "application/vnd.ms-excel")
+		) {
+			setLoading(false);
+			MySwal.fire({
+				title: <p>Only files in .csv format</p>,
+				icon: "error",
+			});
+		} else {
+			try {
+				data = await loadFile(e);
+				e.target.value = null;
+			} catch (error) {
+				setLoading(false);
+				MySwal.fire({
+					title: <p> {error} </p>,
+					icon: "error",
+				});
+				e.target.value = null;
+				return;
+			}
 
-      //setData(data);
-      const resp = await uploadKPIs(data, idccms);
+			//setData(data);
+			const resp = await uploadKPIs(data, idccms);
 
-      if (resp.status === 200) {
-        setLoading(false);
-        MySwal.fire({
-          title: <p>File upload</p>,
-          icon: "success",
-          confirmButtonText: "Accept",
-          allowOutsideClick: false,
-        }).then((resultado) => {
-          if (resultado.value) {
-            window.location.reload();
-          }
-        });
-      }
-    }
-  };
+			if (resp.status === 200) {
+				setLoading(false);
+				MySwal.fire({
+					title: <p>File upload</p>,
+					icon: "success",
+					confirmButtonText: "Accept",
+					allowOutsideClick: false,
+				}).then((resultado) => {
+					if (resultado.value) {
+						window.location.reload();
+					}
+				});
+			}
+		}
+	};
 
-  const downloadFile = () => {
-    var link = document.createElement("a");
-    link.setAttribute("download", "Upload KPI template");
-    //link.href = Quiz_template;
-    link.href = downloadTemplate;
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-  };
-
-  return (
-    <MainPage minHeight="100vh">
-      {loading && <ModalLoading />}
-      <Header />
-      <Typography variant="h5">Kpi's Upload Section</Typography>
-      <BoxButton>
-        <ButtonAction startIcon={<FiDownload />} onClick={() => downloadFile()}>
-          Download Template
-        </ButtonAction>
-        <BoxUpFile>
-          <label htmlFor="kpi">
-            <FiUpload size={20} />
-            Upload
-          </label>
-          <input
-            type="file"
-            id="kpi"
-            name="kpi"
-            onChange={(e) => uploadFile(e)}
-          />
-        </BoxUpFile>
-      </BoxButton>
-      <Grid container>
-        <Grid item xs={12} md={8}>
-          <Item>
-            <TableKPIUpload dataKPI={dataKpi} tableLoading={tableLoading} />
-          </Item>
-        </Grid>
-      </Grid>
-      <Footer />
-    </MainPage>
-  );
+	return (
+		<MainPage minHeight="100vh">
+			{loading && <ModalLoading />}
+			<Header />
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<ModalBox sx={{ width: { xs: "390px", md: "600px", lg: "780px" } }}>
+					<UpQuizModal handleClose={handleClose} template={template} />
+				</ModalBox>
+			</Modal>
+			<Typography variant="h5">Kpi's Upload Section</Typography>
+			<BoxButton>
+				<ButtonAction startIcon={<FiDownload />} onClick={handleOpen}>
+					Download Template
+				</ButtonAction>
+				<BoxUpFile>
+					<label htmlFor="kpi">
+						<FiUpload size={20} />
+						Upload
+					</label>
+					<input
+						type="file"
+						id="kpi"
+						name="kpi"
+						onChange={(e) => uploadFile(e)}
+					/>
+				</BoxUpFile>
+			</BoxButton>
+			<Grid container>
+				<Grid item xs={12} md={8}>
+					<Item>
+						<TableKPIUpload dataKPI={dataKpi} tableLoading={tableLoading} />
+					</Item>
+				</Grid>
+			</Grid>
+			<Footer />
+		</MainPage>
+	);
 };
 
 export default KpiUpload;
