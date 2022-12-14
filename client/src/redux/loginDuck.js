4f6358fb72a07df8fff2bca0b1b0ae9fe922fdbe
Deiby Nino Garces <ninogarces.5@teleperformance.co>
2022-12-14 10:08:31 -0500

    merge Testing 141222

--- client/src/redux/loginDuck.js
+++ client/src/redux/loginDuck.js
@@ -7,13 +7,13 @@ import CryptoJS from "crypto-js";
 //const url = "http://localhost:4343";
 
 // Desarrollo - testing
-const url = "https://gamificationtest.teleperformance.co";
+//const url = "https://gamificationtest.teleperformance.co";
 //const url = "https://gptest.teleperformance.co";
 // Pilot
 //const url = "https://spacegptest.teleperformance.co";
 //const url = "http://10.138.143.224:4343";
 //Produccion
-//const url = "https://spacegp.teleperformance.co";
+const url = "https://spacegp.teleperformance.co";
 
 //datainicial
 const initialData = {
