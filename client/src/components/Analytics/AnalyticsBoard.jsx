83a19f360dd3f24b874175f06dbf3166a66f713f
Alejandro Tapias <tapiaspinzon.5@nlsa.teleperformance.co>
2023-04-24 13:01:25 -0500

    BE push

--- client/src/components/Analytics/AnalyticsBoard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MainPage } from "../../assets/styled/muistyled";
-
-const AnalyticsBoard = () => {
-  return <MainPage>AnalyticsBoard</MainPage>;
-};
-
-export default AnalyticsBoard;
